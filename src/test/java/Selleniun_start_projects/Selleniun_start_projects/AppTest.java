0c17e60fcd4b59e11b6f42685f7967d39d4413d2
Amer Said <156580420+amersaid811@users.noreply.github.com>
2024-04-03 16:13:30 +0300

    password validation tests

--- src/test/java/Selleniun_start_projects/Selleniun_start_projects/AppTest.java
+++ src/test/java/Selleniun_start_projects/Selleniun_start_projects/AppTest.java
@@ -24,7 +24,7 @@ public class AppTest {
 	 @Before
 	  public void setup() {
 		 
-			  System.setProperty("webdriver.gecko.driver","C:\\geckodriver-v0.34.0-win32\\geckodriver.exe");
+			  System.setProperty("webdriver.gecko.driver","C:\\Users\\HCnet\\git\\Selleniun_start_projects\\drivers\\geckodriver.exe");
 		      browser = new FirefoxDriver();
 		  	  browser.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
 			
@@ -49,6 +49,22 @@ public class AppTest {
 			String newPage = browser.getCurrentUrl();
 			assertNotEquals(currentPage, newPage);
     }
-	
+	@Test
+	public void Tests2() {
+    	
+		
+			WebElement textBox = browser.findElement(By.id("firstname"));
+			textBox.sendKeys("Amer");
+			textBox = browser.findElement(By.id("surname"));
+			textBox.sendKeys("Zilberstein");
+			textBox = browser.findElement(By.id("age"));
+			textBox.sendKeys("21");
+			Select drpCountry = new Select(browser.findElement(By.name("country")));
+			drpCountry.selectByVisibleText("Israel");
+			String currentPage=browser.getCurrentUrl();
+			browser.findElement(By.cssSelector("input[type='submit']")).click();
+			String newPage = browser.getCurrentUrl();
+			assertNotEquals(currentPage, newPage);
+    }
 	
 }
